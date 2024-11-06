ba259e3a2852cccf253faf06f53f6e7425a7b71a
prowork-sudo <preran.work@gmail.com>
2024-11-06 17:59:31 +0000

    day6

---
+++ Day6.py
@@ -0,0 +1,38 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def hop():
+#     turn_left()
+#     move()
+#     while wall_on_right() == True:
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+# while at_goal() == False:
+#     if wall_in_front() == True:
+#         hop()
+#     else:
+#         move()
+
+
+
+
+# maze
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
