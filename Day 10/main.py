756603c49e0b354b0c8985643c88208db0af5610
prowork-sudo <preran.work@gmail.com>
2024-11-10 18:25:57 +0000

    day10

---
+++ Day 10/main.py
@@ -0,0 +1,41 @@
+def add(a,b):
+    return a + b
+
+def sub(a,b):
+    return a - b
+
+def multiply(a,b):
+    return a * b
+
+def divide(a,b):
+    return a / b
+
+
+methods = {"+":add,
+            "-":sub,
+            "*":multiply,
+             "/":divide
+}
+def calc():
+    cont = True
+    cont_new = True
+
+    while cont_new == True:
+        first = int(input("what is your first number? \n"))
+        cont_new = False
+    while cont == True:
+        operation = input("Pick an operation \n")
+
+        second = int(input("what is nest number \n"))
+        new_first = methods[operation](first,second)
+        print(new_first)
+
+        a = input(f"type y to continue with {new_first} or type n to go with new number ")
+        if a =="y":
+            cont_new = False
+            first = new_first
+        else:
+            cont_new = True
+            calc()
+
+calc()
