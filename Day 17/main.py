ad6bc1e9380240c1b204b133d39040f498967220
prowork-sudo <preran.work@gmail.com>
2024-11-21 12:07:16 +0000

    day17

--- Day 17/main.py
+++ Day 17/main.py
@@ -1,1 +1,20 @@
-print("sorry")
+class User:
+    def __init__(self, id, username): # Class constructor with attributes
+        self.id = id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+
+    def follow(self, user): # Method
+        user.followers += 1
+        self.following +=1
+
+
+user_1 = User("111","yoyoyoyuiyo")
+user_2 = User("112","yoyofbhafdvskjhbyoyuiyo")
+
+user_1.follow(user_2)
+
+print(user_1.following)
+print(user_2.followers)
