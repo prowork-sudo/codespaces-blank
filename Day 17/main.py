c3a8ceba8e80945437aace1f53b349d6aa1fb913
prowork-sudo <preran.work@gmail.com>
2024-11-26 00:20:56 +0530

    day17

--- Day 17/main.py
+++ Day 17/main.py
@@ -1,21 +1,26 @@
-class User:
-    def __init__(self, id, username): # Class constructor with attributes
-        self.id = id
-        self.username = username
-        self.followers = 0
-        self.following = 0
+# class User:
+#     def __init__(self, id, username): # Class constructor with attributes
+#         self.id = id
+#         self.username = username
+#         self.followers = 0
+#         self.following = 0
 
 
-    def follow(self, user): # Method
-        user.followers += 1
-        self.following +=1
+#     def follow(self, user): # Method
+#         user.followers += 1
+#         self.following +=1
 
 
-user_1 = User("111","yoyoyoyuiyo")
-user_2 = User("112","yoyofbhafdvskjhbyoyuiyo")
+# user_1 = User("111","yoyoyoyuiyo")
+# user_2 = User("112","yoyofbhafdvskjhbyoyuiyo")
 
-user_1.follow(user_2)
+# user_1.follow(user_2)
 
-print(user_1.following)
-print(user_2.followers)
-print("change")
+# print(user_1.following)
+# print(user_2.followers)
+# print("change")
+
+class question():
+    def __init__(self, q_question, q_text):
+        self.question = q_question
+        self.text =  q_text
