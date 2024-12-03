07a36d060e8a063a78f6edd490f46de6ef142e24
prowork-sudo <preran.work@gmail.com>
2024-12-03 23:20:28 +0530

    day17

--- Day 17/main.py
+++ Day 17/main.py
@@ -20,5 +20,18 @@
 # print(user_2.followers)
 # print("change")
 
-imoprt question from question
-import question_data from data
+from question import Question
+from data import question_data
+from quiz_brain import quiz_brain
+
+question_bank = []
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text,question_answer)
+    question_bank.append(new_question)
+
+quiz = quiz_brain(question_bank)
+while quiz.still_has_question():
+    quiz.next_question()
--- Day 17/question.py
+++ Day 17/question.py
@@ -1,5 +1,4 @@
-
-class question():
-    def __init__(self, q_question, q_text):
-        self.question = q_question
-        self.text =  q_text
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
