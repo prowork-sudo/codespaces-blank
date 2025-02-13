72716e4b132e3c8c898928b5f19624025c6349f9
prowork-sudo <preran.work@gmail.com>
2025-02-13 22:21:38 +0530

    13/02/2025-01

---
+++ Data Structures/Sorting/Bubble Sort/main.py
@@ -0,0 +1,9 @@
+def bubble_sort(my_list):
+    for i in range(len(my_list)-1, 0,-1):
+        for j in range(i):
+            if my_list[j] > my_list[j+1]:
+                my_list[j], my_list[j+1] = my_list[j+1], my_list[j]
+    return my_list
+
+my_list = [5,4,3,2,1]
+print(bubble_sort(my_list))
