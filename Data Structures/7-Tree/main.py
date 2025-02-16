dc151b5bd0c08b1be77c89fe4acbe3ec865a06f4
prowork-sudo <preran.work@gmail.com>
2025-02-16 21:35:30 +0530

    16/02/2025

---
+++ Data Structures/7-Tree/main.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class binary_search_tree():
+    def __init__(self):
+        self.root = None
+        
