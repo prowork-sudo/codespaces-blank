9d8e3fb1710bda446f386073d5047d2dc071a9d9
prowork-sudo <preran.work@gmail.com>
2025-02-17 23:55:54 +0530

    17/02/2025

--- Data Structures/7-Tree/main.py
+++ Data Structures/7-Tree/main.py
@@ -1,10 +1,64 @@
-class Node:
+from logging import root
+
+
+class Node():
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
+'''Class binary_search_tree():
+    def __init__(self,value):
+        new_node = Node(value)
+        self.root = new_node '''
+
 class binary_search_tree():
     def __init__(self):
         self.root = None
-        
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while (True):
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+    def contains(self, value):
+        temp = self.root
+        while temp is not None:
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else: return True
+        return False
+
+
+
+             
+
+            
+my_tree = binary_search_tree()
+my_tree.insert(5)
+print(my_tree.root.value)
+my_tree.insert(1)
+print(my_tree.root.left.value)
+my_tree.insert(9)
+print(my_tree.root.right.value)
+print(my_tree.contains(0))
+
+
+# BST traversal methods
---
+++ Data Structures/8-Hash/main.py
@@ -0,0 +1,53 @@
+#good to have prime number
+from re import S
+
+
+class HashTable:
+    def __init__(self, size=7):
+        self.data_map = [None] * 7
+
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash
+    
+    def print_table(self):
+        for i , value in enumerate(self.data_map):
+            print(i, ":", value)
+    
+    def set_item(self, key, value):
+        index = self.__hash(key)
+        if self.data_map[index] is None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+    
+    def get_item(self, key):
+        index = self.__hash(key)
+        while index:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
+    
+    def key(self):
+        all_keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys
+
+    
+my_hast_table = HashTable()
+my_hast_table.set_item('hey', 1500)
+my_hast_table.set_item('bay', 150)
+my_hast_table.set_item('cay', 1600)
+my_hast_table.set_item('day', 100)
+my_hast_table.set_item('may', 1505)
+print(my_hast_table.get_item('cay'))
+print(my_hast_table.key())
+my_hast_table.print_table()
+
+
+        
